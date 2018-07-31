22d2905682fe498c02a0bd65a20e69725b66440a
Demo User <demouser@MacBook-Pro.local>
2018-07-30 17:50:06 -0700

    created login & logout link (without post method) and linked the welcome, results, & recipe instructions html pages

--- main.py
+++ main.py
@@ -2,28 +2,76 @@
 import webapp2
 #libraries for APIs
 from google.appengine.api import urlfetch
+from google.appengine.api import users
+from google.appengine.ext import ndb
 import json
+import jinja2
+import os
 
+jinja_current_directory  = jinja2.Environment(
+    loader=jinja2.FileSystemLoader(os.path.dirname(__file__)),
+    extensions=['jinja2.ext.autoescape'],
+    autoescape=True)
 
-class MainPage(webapp2.RequestHandler):
+# class Event(ndb.Model):
+#     organizer = ndb.StringProperty(required=True)
+#     title = ndb.StringProperty(required=True)
+#
+class Event(ndb.Model):
+    organizer = ndb.StringProperty(required=True)
+    title = ndb.StringProperty(required=True)
+
+class WelcomePage(webapp2.RequestHandler):
 
     def get(self):
-        global APP_ID
-        APP_ID = ""
-        global APP_KEY
-        APP_KEY = ""
-        search_endpoint_url = "https://api.edamam.com/search?q=chicken&app_id=" + APP_ID +"&app_key=" + APP_KEY
-        search_response = urlfetch.fetch(search_endpoint_url).content
-        print search_response
-        recipes_as_json = json.loads(search_response)
-        self.response.write(recipes_as_json)
+        logout_link = users.create_logout_url('/')
+        start_template = jinja_current_directory.get_template('templates/welcome.html')
+        self.response.write(start_template.render({
+            'logout_link': logout_link
+        }))
+
     def post(self):
-      
-        self.response.write(correct_answer)
+        user = users.get_current_user()
+        if user:
+            nickname = user.nickname()
+            logout_url = users.create_logout_url('/')
+            greeting = 'Welcome, %s! (<a href="%s">sign out</a>)'.format(
+                nickname, logout_url)
+            add_template = jinja_current_directory.get_template('templates/welcome.html')
+            self.response.write(add_template.render({'templates': templates}))
+        else:
+            login_prompt_template = jinja_current_directory.get_template('templates/login-please.html')
+            self.response.write(login_prompt_template.render({
+                'login_link': users.create_login_url('/')
+                }))
 
+        self.response.write(
+            'hello'.format(greeting))
+        # self.response.write(correct_answer)
+        #
+        # welcome_template = JINJA_ENVIRONMENT.get_template('templates/welcome.html')
+        # self.response.write(welcome_template.render(template_vars))
 
+class ResultsPage(webapp2.RequestHandler):
+    def get(self):
+        results_template = jinja_current_directory.get_template('templates/results.html')
+        self.response.write(results_template.render())
 
+class RecipeInstructionsPage(webapp2.RequestHandler):
+    def get(self):
+        recipe_instructions_template = jinja_current_directory.get_template('templates/recipe-instructions.html')
+        self.response.write(recipe_instructions_template.render())
+
+class LoginPlease(webapp2.RequestHandler):
+    def get(self):
+        login_prompt_template = jinja_current_directory.get_template('templates/login-please.html')
+        self.response.write(login_prompt_template.render({
+            'login_link': users.create_login_url('/')
+            }))
 
 app = webapp2.WSGIApplication([
-    ('/', MainPage),
+    ('/', WelcomePage),
+    ('/results', ResultsPage),
+    ('/RecipeInstructionsPage', RecipeInstructionsPage),
+    ('/LoginPlease', LoginPlease)
 ], debug=True)
