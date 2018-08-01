874c7aa01dfd9090b915eaa6470f6cbe7e3fc4e8
Sammy Mohammed <sammymohammed00@gmail.com>
2018-08-01 14:28:43 -0700

    WIP favorite system

--- main.py
+++ main.py
@@ -25,53 +25,10 @@ class WelcomePage(webapp2.RequestHandler):
     def get(self):
         welcome_template = jinja_current_directory.get_template('templates/welcome.html')
         self.response.write(welcome_template.render({'login_url': users.create_login_url('/main')}))
-
     def post(self):
-        #recipe API
-        global APP_ID
-        APP_ID = SECRETS_DICT['api-id1']
-        global APP_KEY
-        APP_KEY = "84103e81ef614ae430c3bc458d36ff16"
-        urlfetch.set_default_fetch_deadline(60) #this sets the deadline
-        url=("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=" + urllib.quote(self.request.get("foodlist").replace(" ", "")) + "&number=5&ranking=2")
-        print url
-        result = urlfetch.fetch( #this goes to the endpoint and grabs the json
-              # url="https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=" + urllib.quote(self.request.get("foodlist")) + "&number=5&ranking=1",
-              url,
-              headers={
-                "X-Mashape-Key": SECRETS_DICT['mashape-key'],
-                "X-Mashape-Host": SECRETS_DICT['mashape-host'],
-              },)
-        print url
+        self.redirect("/main")
 
-        recipe_list = json.loads(result.content)
-        #print recipe_list #parses json
-        all_recipe_names = []
-        all_recipe_images = []
-        for recipenames in recipe_list:
-            all_recipe_names.append(recipenames["title"])
 
-        #print all_recipe_names[0]
-        final_recipe_dict = {}
-
-        for recipe in all_recipe_names:
-            recipe = urllib.quote(recipe)
-        #    print recipe
-            recipe_search_info = urlfetch.fetch("https://api.edamam.com/search?q=" + recipe + "&app_id=" + APP_ID +"&app_key=" + APP_KEY + "&to=1")
-            print json.loads(recipe_search_info.content)["hits"][0]["recipe"]["url"]
-            final_recipe_dict[recipe] = json.loads(recipe_search_info.content)["hits"][0]["recipe"]["url"]
-            all_recipe_images.append(json.loads(recipe_search_info.content)["hits"][0]["recipe"]["image"])
-
-        tempval = 0
-        for key in final_recipe_dict:
-            self.response.write("<br> <img src=" + all_recipe_images[tempval] + "> <br> " + key.replace("%20", " ") + ". Find more information at: " + final_recipe_dict[key])
-            tempval += 1
-
-        template_vars = {
-            'input_ingredient': self.request.get('foodlist'),
-        }
-        welcome_template = jinja_current_directory.get_template('templates/results.html')
-        self.response.write(welcome_template.render(template_vars))
 
 class MainPage(webapp2.RequestHandler):
       def get(self):
@@ -88,12 +45,57 @@ class MainPage(webapp2.RequestHandler):
             # if (user_models.User.query().filter(user_models.User.nickname == nickname).fetch()[0] == nickname):
             #     pass
             # else: WIP MAKING SURE YOU CANT REGISTER TWICE BC RN YOU CAN
-            user_models.User(user_id=user_id, nickname=nickname).put()
+            global current_user_key
+            current_user_key = user_models.User(user_id=user_id, nickname=nickname).put()
         else:
             self.redirect('/welcome')
         main_template = jinja_current_directory.get_template('templates/main.html')
         self.response.write(main_template.render(template_var))
 
+      def post(self):
+          global APP_ID
+          APP_ID = SECRETS_DICT['api-id1']
+          global APP_KEY
+          APP_KEY = SECRETS_DICT['api-key1']
+          urlfetch.set_default_fetch_deadline(60) #this sets the deadline
+          url=("https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=" + urllib.quote(self.request.get("foodlist").replace(" ", "")) + "&number=5&ranking=2")
+          print url
+          result = urlfetch.fetch( #this goes to the endpoint and grabs the json
+                # url="https://spoonacular-recipe-food-nutrition-v1.p.mashape.com/recipes/findByIngredients?ingredients=" + urllib.quote(self.request.get("foodlist")) + "&number=5&ranking=1",
+                url,
+                headers={
+                    "X-Mashape-Key": SECRETS_DICT['mashape-key'],
+                    "X-Mashape-Host": SECRETS_DICT['mashape-host'],
+                    },)
+          print url
+
+          recipe_list = json.loads(result.content)
+          #print recipe_list #parses json
+          all_recipe_names = []
+          all_recipe_images = []
+          for recipenames in recipe_list:
+              all_recipe_names.append(recipenames["title"])
+
+              #print all_recipe_names[0]
+          final_recipe_dict = {}
+
+          for recipe in all_recipe_names:
+              recipe = urllib.quote(recipe)
+              recipe_search_info = urlfetch.fetch("https://api.edamam.com/search?q=" + recipe + "&app_id=" + APP_ID +"&app_key=" + APP_KEY + "&to=1")
+              print json.loads(recipe_search_info.content)["hits"][0]["recipe"]["url"]
+              final_recipe_dict[recipe] = json.loads(recipe_search_info.content)["hits"][0]["recipe"]["url"]
+              all_recipe_images.append(json.loads(recipe_search_info.content)["hits"][0]["recipe"]["image"])
+
+          tempval = 0
+          for key in final_recipe_dict:
+              self.response.write('<form action=/favorites method="POST"><br> <input type="hidden" name="key" value="' + str(users.get_current_user().user_id())  + '"/><input type="hidden" name="title" value="' + key + '"> <input type="hidden" name="image" value="' + all_recipe_images[tempval] + '"/> <input type="hidden" name="url" value="' + final_recipe_dict[key] + '"/> <img src=' + all_recipe_images[tempval] + '> <br> ' + key.replace("%20", " ") + ". Find more information at: " + final_recipe_dict[key] + '<input type="submit"/>')
+              tempval += 1
+
+          template_vars = {
+            'input_ingredient': self.request.get('foodlist'),
+            }
+          welcome_template = jinja_current_directory.get_template('templates/results.html')
+          self.response.write(welcome_template.render(template_vars))
         #recipe API-----------------
     #     global APP_ID
     #
@@ -122,13 +124,20 @@ class MainPage(webapp2.RequestHandler):
     #     template_vars = {
     #         'input_ingredients': self.request.get('input_ingredients'),
     #     }
-class RecipeInstructionsPage(webapp2.RequestHandler):
+class FavoritesPage(webapp2.RequestHandler):
     def get(self):
         recipe_instructions_template = jinja_current_directory.get_template('templates/recipe-instructions.html')
         self.response.write(recipe_instructions_template.render())
 
+    def post(self):
+        user_model_key = user_models.Recipe(
+            title=self.request.get("title"), image=self.request.get("image"),
+            url=self.request.get("url")).put()
+        user_models.User.query().filter(user_models.User.user_id == self.request.get("key")).get().recipes = (user_model_key,)
+
+
 app = webapp2.WSGIApplication([
     ('/', WelcomePage),
     ('/main', MainPage),
-    ('/RecipeInstructionsPage', RecipeInstructionsPage),
+    ('/favorites', FavoritesPage),
 ], debug=True)
